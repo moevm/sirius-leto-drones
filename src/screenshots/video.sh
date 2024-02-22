30d08a077373d9d659302391a12dcc36cb9b7b68
Konstantin Evgenievich Kondratenko <90711883+KonstantinKondratenko@users.noreply.github.com>
2024-02-22 21:11:00 +0300

    upd video results

--- src/screenshots/video.sh
+++ src/screenshots/video.sh
@@ -6,9 +6,9 @@
 # 1) chmod +x ./run_mp.sh
 # 2_ run the script to create "video.mp4" in the same directory: $ ./run_mp.sh
 
-FRAME_RATE=5
+FRAME_RATE=10
 RESOLUTION=640x480
 
-ffmpeg -r $FRAME_RATE -f image2 -s $RESOLUTION -i frame_%d.png -vcodec libx264 -crf 25  -pix_fmt yuv420p video.mp4
+ffmpeg -r $FRAME_RATE -f image2 -s $RESOLUTION -i frame_%d.png -vcodec libx264 -crf 25  -pix_fmt yuv420p -y video.mp4
 rm *.png
 mpv video.mp4 
